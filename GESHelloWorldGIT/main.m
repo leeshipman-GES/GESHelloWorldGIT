c5401a1c8c9356e45eacb781ced7b1e5c9966a56
Lee Shipman <leeshipman@mac.com>
2012-12-02 11:13:38 -0500

    Initial Commit

---
+++ GESHelloWorldGIT/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GESHelloWorldGIT
+//
+//  Created by Lee Main on 12/2/12.
+//  Copyright (c) 2012 GES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
